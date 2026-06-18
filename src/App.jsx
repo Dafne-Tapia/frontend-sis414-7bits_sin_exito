c706f39045632fce4b7dc90647df143974e91f66
Dafne Tapia <romidafne23@gmail.com>
2026-06-18 16:23:50 -0400

    hacer avance de mi frontend entidad, aun falta mejoras de colores, por lo demas da muy bien¨

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import NavBar from './components/NavBar'
-import Sidebar from './components/Sidebar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Entidad from './pages/Entidad'
 
 function App() {
   return (
     <BrowserRouter>
-            <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/entidad" element={<Entidad />} />
+      </Routes>
     </BrowserRouter>
   )
 }
