31c11b386c65ac408d5a47bad02332b7b5269cdc
Dafne Tapia <romidafne23@gmail.com>
2026-06-25 03:08:12 -0400

    agregar logo del grupo en encabezado, en home y entidad frontend

--- src/pages/Entidad.jsx
+++ src/pages/Entidad.jsx
@@ -131,6 +131,12 @@ function Entidad() {
             <h1 className="titulo-marca">V.S.I.A.F</h1>
             <p className="subtitulo-marca">Sistema de Activos Fijos</p>
           </div>
+          {/* aqui agregar logo grupal */}
+            <img
+               src="/src/assets/logo_grupo.jpeg"
+               alt="Logo 7 Bits Sin Éxito"
+               className="logo-grupo"
+            />
         </div>
       </header>
 
