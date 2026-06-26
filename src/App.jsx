c0a7e3c7ccf85c58a075b6fb2a783b09c086cdcc
Lhw819 <hw81zzz9@gmail.com>
2026-06-26 06:25:43 -0400

    mes frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-
 import Entidad from './pages/Entidad'
 import UnidadAdmin from './pages/UnidadAdmin'
+import Mes from './pages/Mes'
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/entidad" element={<Entidad />} />
         <Route path="/unidad-administrativa" element={<UnidadAdmin />} />
+        <Route path="/mes" element={<Mes />} />
       </Routes>
     </BrowserRouter>
   )
