9e2035db99e243add0d63e10d6742d6fb23860db
anahimartineztaboada7240-web <anahi.martinez.taboada7240@gmail.com>
2026-06-26 07:32:36 -0400

    trabajo avanzado

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Home from './pages/Home'
 
 import Entidad from './pages/Entidad'
 import UnidadAdmin from './pages/UnidadAdmin'
+import ObjGasto from './pages/ObjGasto'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/entidad" element={<Entidad />} />
-        <Route path="/unidad-administrativa" element={<UnidadAdmin />} />
+          <Route path="/entidad" element={<Entidad />} />
+          <Route path="/unidad-administrativa" element={<UnidadAdmin />} />
+          <Route path="/obj-gasto" element={<ObjGasto />} />
       </Routes>
     </BrowserRouter>
   )
