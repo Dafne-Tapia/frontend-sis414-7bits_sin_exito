464ca95517cad60757f979db164b60ea92d85f2a
kevin-alex-aguirre-choque <ka2670984@gmail.com>
2026-06-27 00:51:19 -0400

    mejoras

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import UnidadAdmin from './pages/UnidadAdmin'
 function App() {
   return (
     <BrowserRouter>
-  
+
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/entidad" element={<Entidad />} />
