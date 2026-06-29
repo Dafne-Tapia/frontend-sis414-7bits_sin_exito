f568f9d5b8c5de044ba465a4a4c2f042f0e65746
Lhw819 <hw81zzz9@gmail.com>
2026-06-28 20:04:42 -0400

    Problema con Sidebar soliucionado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,10 +7,15 @@ function Sidebar() {
       <ul className="sidebar-list">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/entidad">Entidad</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/obj-gasto">Objeto de Gasto</Link></li>
+        <li><Link to="/unidad-administrativa">Unidad Administrativa</Link></li>
         <li><Link to="/mes">Mes</Link></li>
+        <li><Link to="/estado">Estado</Link></li>
+        <li><Link to="/baja">Baja</Link></li>
+        <li><Link to="/cta-par">Cta Par</Link></li>
       </ul>
     </aside>
   )
 }
+
 export default Sidebar
